bcd3b09a464ec0d3e68ad0ce188f11dc15ee0877
RezaulAlamOni <rzalam93@gmail.com>
2021-04-23 10:28:31 +0600

    Update tonoyav1 android home

--- resources/views/backend/android_home.blade.php
+++ resources/views/backend/android_home.blade.php
@@ -64,6 +64,19 @@
                             </div>
                         </a>
 
+                        <a  href="#" class="btn btn-default">
+                            <div class="numbering_col" style="padding: 0; margin: 0;">9</div>
+                            <div class="col-md-11 text-center" style="padding: 0; margin: 0;line-height:35px;">
+                                入数設定
+                            </div>
+                        </a>
+
+                        <a  href="{{Config::get('app.url').'/stock_details_by_handy'}}" class="btn btn-default">
+                            <div class="numbering_col" style="padding: 0; margin: 0;">11</div>
+                            <div class="col-md-11 text-center" style="padding: 0; margin: 0;line-height:35px;">
+                                商品検索
+                            </div>
+                        </a>
                     </div>
 
                     <div class="col-6 handy_btn_aria handy_btn_right">
@@ -102,6 +115,27 @@
                         <a class="btn btn-default" id="jan_manter_handy_btn_delivery" href="handy_customer_master">
                             <div class="numbering_col" style="padding: 4px; margin: 0;">6</div>
                             <div class="col-md-11 text-center" style="padding: 0; margin: 0;line-height:35px;">販売先マスター</div>
+                        </a>
+                        <a id="delivery_inventorys" class="btn btn-default" href="{{Config::get('app.url').'/handy_order_shipment_list'}}">
+                            <div class="numbering_col" style="padding: 0; margin: 0;">8</div>
+                            <div class="col-md-11 text-center" style="padding: 0; margin: 0;line-height:35px;">
+                                出荷・確定・後(問屋)
+                            </div>
+                        </a>
+
+                        <a id="handy_order_btn" style="width: 100%" class="btn btn-default" href="handy_store_order">
+                            <div class="numbering_col" style="padding: 0; margin: 0;">10</div>
+                            <div class="col-md-11 text-center" style="padding: 0; margin: 0;line-height:35px;">
+                                発注(小売用・問屋)
+
+                            </div>
+                        </a>
+                        <a id="" style="width: 100%" class="btn btn-default" href="#">
+                            <div class="numbering_col" style="padding: 0; margin: 0;">12</div>
+                            <div class="col-md-11 text-center" style="padding: 0; margin: 0;line-height:35px;">
+                                問屋発注(小売・問屋)
+
+                            </div>
                         </a>
                     </div>
                 </div>
