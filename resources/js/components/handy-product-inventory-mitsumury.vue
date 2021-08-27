154405b5da315b6494e5407437aef5314d7fdad1
RezaulAlamOni <rzalam93@gmail.com>
2021-08-27 16:31:50 +0600

    update code for mitsumury page title

--- resources/js/components/handy-product-inventory-mitsumury.vue
+++ resources/js/components/handy-product-inventory-mitsumury.vue
@@ -6,8 +6,8 @@
                 <div class="well" style="border: 3px solid #428bca;">
                     <div class="header col-md-12 col-xs-12" style="font-size: 18px; padding: 10px;">
                         <span class="pull-left">
-                                在庫問い合わせ M
-                            </span>
+                                見積り
+                        </span>
                         <!-- <button id="handy_shipment_item_insert" class="btn btn-primary pull-right" style="float:right"> 送信</button>&nbsp;-->
                         <a :href="base_url+'/android_home'" class="btn btn-primary pull-right"
                            style="float:right"> メニュー</a>
