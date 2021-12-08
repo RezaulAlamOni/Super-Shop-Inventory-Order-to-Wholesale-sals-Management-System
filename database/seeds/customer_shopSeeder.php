280af8ac473209a2c90402ccccea94f49c8acf7f
a_sakil <jacossakil@gmail.com>
2021-12-08 20:05:02 +0600

    user type added and seeder shop user added

--- database/seeds/customer_shopSeeder.php
+++ database/seeds/customer_shopSeeder.php
@@ -15,6 +15,7 @@ class customer_shopSeeder extends Seeder
             [
                 'shop_no' => '123456',
                 'customer_id' => '1',
+                'user_id' => '9',
                 'shop_name' =>'店 A',
                 'shop_address' =>'Tokyo',
                 'shop_postal_code' =>'2233',
@@ -25,30 +26,33 @@ class customer_shopSeeder extends Seeder
             [
                 'shop_no' => '654321',
                 'customer_id' => '1',
+                'user_id' => '10',
                 'shop_name' =>'店 B',
                 'shop_address' =>'Tokyo',
                 'shop_postal_code' =>'2233',
                 'phone' =>'01936755674',
-                'email' =>'shop_a@gmail.com',
+                'email' =>'shop_b@gmail.com',
                 'delivery_cycle' =>'1'
             ],[
                 'shop_no' => '333222',
                 'customer_id' => '2',
+                'user_id' => '11',
                 'shop_name' =>'店 A',
                 'shop_address' =>'Tokyo',
                 'shop_postal_code' =>'2233',
                 'phone' =>'01936755674',
-                'email' =>'shop_a@gmail.com',
+                'email' =>'shop_c@gmail.com',
                 'delivery_cycle' =>'1'
             ],
             [
                 'shop_no' => '222333',
                 'customer_id' => '2',
+                'user_id' => '12',
                 'shop_name' =>'店 B',
                 'shop_address' =>'Tokyo',
                 'shop_postal_code' =>'2233',
                 'phone' =>'01936755674',
-                'email' =>'shop_a@gmail.com',
+                'email' =>'shop_d@gmail.com',
                 'delivery_cycle' =>'1'
             ]
         );
